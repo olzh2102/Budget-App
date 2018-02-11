5b5743cc882684302f681bdd2aae6d211c02cc61
olzhas2102 <olzhas2102@yahoo.com>
2018-02-11 20:36:24 +0600

    Setting up update percentages function

--- app.js
+++ app.js
@@ -226,6 +226,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 				console.log(budget);
 		};
 
+		var updatePercentages = function() {
+
+				// 1. Calculate percentages
+
+				// 2. Read percentages from the budget controller
+
+				// 3. Update UI with new percentages
+		};
+
 		var ctrlAddItem = function() {
 
 				var input, newItem;
@@ -247,6 +256,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+						// 6. Calculate and update percentages
+						updatePercentages();
         }
 		};
 
@@ -272,6 +284,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 						// 3. Update and show the new budget
 						updateBudget();
+
+						// 4. Calculate and update percentages
+						updatePercentages();
 				}
 		};
 
