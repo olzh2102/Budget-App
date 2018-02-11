e376421b7a1b76cd97f7180930319cad2911ea80
olzhas2102 <olzhas2102@yahoo.com>
2018-02-11 12:58:57 +0600

    Clearing input fields and putting back on focus

--- app.js
+++ app.js
@@ -103,6 +103,18 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+				clearFields: function() {
+						var fields, fieldsArr;
+
+						fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+
+						fieldsArr = Array.prototype.slice.call(fields);
+
+						fieldsArr.forEach(function(current, index, array) {
+								current.value = "";
+						});
+				},
+
 				getDOMStrings: function() {
 					return DOMStrings;
 				}
@@ -139,9 +151,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 				// 3. Add the item to the UI
 				UICtrl.addListItem(newItem, input.type);
 
-				// 4. Calculate the budget
+				// 4. Clear fields
+				UICtrl.clearFields();
 
-				// 5. Display the budget on the UI
+				// 5. Calculate the budget
+
+				// 6. Display the budget on the UI
 		};
 
 		return {
